a5aa45d1e943f5d99a7393746dea8cd9d888740f
Elliot Scott <elliotscott2111@gmail.com>
2026-09-01 22:06:32 +0100

    Added funtionality to the add transactions button in AddTransactionsGUI

--- src/main/java/com/financetracker/AddTransactionGUI.java
+++ src/main/java/com/financetracker/AddTransactionGUI.java
@@ -1,22 +1,19 @@
 package com.financetracker;
 
 import javax.swing.*;
-import javax.xml.crypto.Data;
 import java.awt.*;
+import java.time.LocalDateTime;
 
 public class AddTransactionGUI extends JPanel {
     private final JFrame frame;
-    private final MainPanel mainPanel;
     private final JSpinner amountSpinner;
-    private final JComboBox<String> category;
-    private final JTextField description;
+    private final JComboBox<String> categoryComboBox;
+    private final JTextField descriptionTextField;
     private final JSpinner dateSpinner;
-    private final Profile profile;
 
     public AddTransactionGUI(JFrame frame, MainPanel mainPanel){
         this.frame = frame;
-        this.mainPanel = mainPanel;
-        profile = DatabaseHelper.getProfile();
+        Profile profile = DatabaseHelper.getProfile();
 
         setLayout(new BorderLayout());
 
@@ -41,7 +38,7 @@ public class AddTransactionGUI extends JPanel {
         form.add(new JLabel("Cost £:"), gbc);
 
         amountSpinner = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 10000000.0,
-                1000.0));
+                10.0));
         gbc.gridx = 1;
         gbc.weightx = 1;
 
@@ -53,12 +50,12 @@ public class AddTransactionGUI extends JPanel {
 
         form.add(new JLabel("Category:"), gbc);
 
-        category = new JComboBox<>(new String[]{"Entertainment", "Rent", "Food", "Transport", "Shopping", "Clothes",
+        categoryComboBox = new JComboBox<>(new String[]{"Entertainment", "Rent", "Food", "Transport", "Shopping", "Clothes",
                 "Other"});
 
         gbc.gridx = 1;
 
-        form.add(category, gbc);
+        form.add(categoryComboBox, gbc);
 
         // Debt
         gbc.gridx = 0;
@@ -66,17 +63,17 @@ public class AddTransactionGUI extends JPanel {
 
         form.add(new JLabel("Description:"), gbc);
 
-        description = new JTextField();
+        descriptionTextField = new JTextField();
 
         gbc.gridx = 1;
 
-        form.add(description, gbc);
+        form.add(descriptionTextField, gbc);
 
         // Salary
         gbc.gridx = 0;
         gbc.gridy++;
 
-        form.add(new JLabel("Annual Salary:"), gbc);
+        form.add(new JLabel("Date of transaction:"), gbc);
 
         SpinnerDateModel dateModel = new SpinnerDateModel();
 
@@ -91,14 +88,6 @@ public class AddTransactionGUI extends JPanel {
         form.add(dateSpinner, gbc);
 
         add(form, BorderLayout.CENTER);
-
-
-
-
-
-
-
-
 
 
 
@@ -122,6 +111,32 @@ public class AddTransactionGUI extends JPanel {
             frame.revalidate();
             frame.repaint();
         });
+
+        addTransactionButton.addActionListener(e -> addTransaction());
+
+    }
+
+    private void addTransaction(){
+        int amount = (int) amountSpinner.getValue();
+        String categoryText = (String) categoryComboBox.getSelectedItem();
+        if(categoryText == null){
+            return;
+        }
+        String description = descriptionTextField.getText();
+        if(description.isEmpty() || description.length() > 1000){
+            return;
+        }
+        LocalDateTime date = (LocalDateTime) dateSpinner.getValue();
+        if(date == null){
+            return;
+        }
+        String dateString = date.toString();
+        DatabaseHelper.addTransaction(amount, categoryText, description, dateString);
+
+        MainPanel mainPanel = new MainPanel(frame);
+        frame.setContentPane(mainPanel);
+        frame.revalidate();
+        frame.repaint();
 
     }
 }
